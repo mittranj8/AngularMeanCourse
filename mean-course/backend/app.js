de357470865279288c0c444c4e763a480d2fbf02
Mittranj Pansuriya <mpansuriya@reedassociates.org>
2020-02-05 15:02:43 -0600

    small fix

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-  const posts = req.body;
+  const post = req.body;
   console.log(post);
   res.status(201). json({   // resource was created successfully
     message: "Post added successfully!"
