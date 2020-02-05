96d0b90c2d8de3e058a8988c2bb774f7e47eaa00
Mittranj Pansuriya <mpansuriya@reedassociates.org>
2020-02-05 13:49:20 -0600

    fetched intial posts

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -1,14 +1,24 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First middleware');
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send('Hello from express!');
+app.use("/api/posts", (req, res, next) => {
+  const posts = [
+    {
+      id: "sacscsc",
+      title: "First server-side post",
+      content: "This is coming from the server"
+    },
+    {
+      id: "cscv13c",
+      title: "Second server-side post",
+      content: "This is coming from the server"
+    },
+  ];
+  res.status(200).json({
+    message: 'Posts fetched successfully!',
+    posts: posts
+  });
 });
 
 module.exports = app;
